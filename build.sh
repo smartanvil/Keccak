09da913757627c81c081eeb79980f25479d606fd
Santiago Bragagnolo <santiago.bragagnolo@inria.fr>
2017-06-27 12:14:45 +0200

    msg

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+mkdir build/files -p
+cd build/files 
+wget -O- get.pharo.org/60+vmT | bash
+./pharo-ui Pharo eval --save " Pharo3Theme beCurrent. Metacello new baseline: 'Iceberg'; repository: 'github://npasserini/iceberg';load. Metacello new baseline: 'Keccak'; repository: 'github://sbragagnolo/Keccak/src'; load. "
+cd ..
+rm build.sh 
+cd .. 
